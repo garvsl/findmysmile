75908dd675cb795f380e645acf0f320b6e816963
garvsl <garvsehgal7@gmail.com>
2024-03-30 05:41:15 -0400

    add json

--- llm/embeddings.py
+++ llm/embeddings.py
@@ -4,6 +4,8 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core import StorageContext
 from llama_index.core import SimpleDirectoryReader
 import os
+from llama_index.readers.json import JSONReader
+
 
 def get_mongo_client():
   """Establish connection to the MongoDB."""
@@ -14,9 +16,9 @@ def get_mongo_client():
     mongodb_client = pymongo.MongoClient(mongo_uri)
     store = MongoDBAtlasVectorSearch(client)
     storage_context = StorageContext.from_defaults(vector_store=store)
-    docs = SimpleDirectoryReader(
-    input_files=["./data"]
-    ).load_data()
+    
+    reader = JSONReader()
+    docs = reader.load_data("./data/proc.json")
     index = VectorStoreIndex.from_documents(
         docs, storage_context=storage_context
     )
